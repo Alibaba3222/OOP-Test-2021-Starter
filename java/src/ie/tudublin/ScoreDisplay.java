94c67d1e6bada118be1db7863d9738a2f6314bcf
Alibaba3222 <aliananza1990@gmail.com>
2021-04-21 20:52:29 +0100

    fifth commit

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -1,4 +1,4 @@
-package ie.tudublin;
+	package ie.tudublin;
 
 import java.util.ArrayList;
 
@@ -8,6 +8,9 @@ import processing.core.PApplet;
 public class ScoreDisplay extends PApplet
 {
 	String score = "DEFGABcd";
+	char answer = myStr.charAt(0);
+	system.out.println(answer);
+
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 	
@@ -20,6 +23,23 @@ public class ScoreDisplay extends PApplet
 		int i = c - '0'; // i holds the number 7 (55 - 48) 
 		println(i);
 	}
+
+	ArrayList<Note> notes = new ArrayList<Note>();
+
+	void loadScore();1
+    {
+
+        int i = 0;
+        while(i < score.length())
+        {
+            char c = score.charAt(i);
+            Note n = new Note(c, 1);
+            notes.add(n);
+            i ++;
+		
+		}
+
+	while()
 
 	public void setup() 
 	{
