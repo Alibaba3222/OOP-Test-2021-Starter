3479169762e820dbe141d4c280f3efa275dc5fa6
Alibaba3222 <aliananza1990@gmail.com>
2021-04-21 17:55:02 +0100

    one commit

---
+++ java/src/ie/tudublin/note.java
@@ -0,0 +1,27 @@
+package ie.tudublin;
+
+public class note {
+    private int duration;
+    private int note;
+    
+}
+
+public int getDuration() {
+    returns duration;
+
+}
+
+public void setDuration(int duration) {
+    this.duration = duration;
+
+}
+
+public char getNote() {
+    return note;
+}
+
+public void setNote(char note) {
+     this.note = note;
+}
+
+public
