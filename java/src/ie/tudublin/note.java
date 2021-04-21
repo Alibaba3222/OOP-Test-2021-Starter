4de765bce0b70df20b74fa39d8ef296dd6be631b
Alibaba3222 <aliananza1990@gmail.com>
2021-04-21 18:36:51 +0100

    two commit

--- java/src/ie/tudublin/note.java
+++ java/src/ie/tudublin/note.java
@@ -1,27 +1,51 @@
 package ie.tudublin;
 
-public class note {
+import processing.data.TableRow;
+
+public class Note {
+    private String note;
     private int duration;
-    private int note;
-    
+    private String Type;
+
+    public String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
+    public int getDuration() {
+        return duration;
+
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+
+    }
+    public String getType() {
+        return Type;
+
+    }
+    public void setType(String type) {
+        Type = type;
+
+    }
+    public Note(String note, int duration, String type) {
+        this.note = note;
+        this.duration = duration;
+        this.Type = type;
+
+    }
+    @Override
+    public String toString() {
+        return "Note [Type=" + Type + ", duration=" + duration + ", note=" + note + "]";
+
+    }
+
+    public Note(TableRow r)
+    {
+        this(r.getString("note"),r.getInt("duration"),r.getString("Type"));
+        
+    }
+
+
 }
-
-public int getDuration() {
-    returns duration;
-
-}
-
-public void setDuration(int duration) {
-    this.duration = duration;
-
-}
-
-public char getNote() {
-    return note;
-}
-
-public void setNote(char note) {
-     this.note = note;
-}
-
-public
